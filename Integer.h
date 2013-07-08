ebf0a8a59270ed1f7771d0b833ac6a2ba9d24b3d
Aashish Sheshadri <aashish.sheshadri@gmail.com>
2013-07-08 02:25:21 -0500

    Adding division

--- Integer.h
+++ Integer.h
@@ -306,8 +306,8 @@ bool myCompare (II1 b1, II1 e1, II2 b2, II2 e2, unsigned int skip){
         if(moveNum && *b1<*b2){
             std::cout<<std::endl<<*b1<<"<"<<*b2;
             return false;
-        }else{
-            std::cout<<std::endl<<*b1<<">="<<*b2;
+        }else if(moveNum && *b1>*b2){
+            std::cout<<std::endl<<*b1<<">"<<*b2;
             return true;}
         ++b1;
         if(!moveNum)
