b1402d0dd56f87e7272f238c1dd771b4097605a4
Aashish Sheshadri <aashish.sheshadri@gmail.com>
2013-07-10 10:19:50 -0500

    adding pow

--- Integer.h
+++ Integer.h
@@ -937,7 +937,7 @@ class Integer {
            //     std::cout<<*it<<" "<<*itBTemp<<std::endl;
             for(deque<bool>::iterator itB = powersOdd.begin();itB!=powersOdd.end();++itB){
                 products.push_back(*this);
-                typename C::iterator temp = this->_integer.begin();
+                //typename C::iterator temp = this->_integer.begin();
                 //while(temp!=this->_integer.end()){
                 //    std::cout<<*temp<<" ";
                 //    ++temp;}
