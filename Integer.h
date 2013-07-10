10a45cecca43aebcd79cf8b0b3028e042cfe2a28
Aashish Sheshadri <aashish.sheshadri@gmail.com>
2013-07-10 00:16:45 -0500

    adding pow

--- Integer.h
+++ Integer.h
@@ -880,6 +880,9 @@ class Integer {
                     powers.push_back(--e/2);
                     powersOdd.push_back(true);
                     e/=2;}}
+            std::deque<bool> itBTemp = powersOdd.begin();
+            for(std::deque<int>::iterator it = powers.begin();it!=powers.end();++it,++itBTemp)
+                std::cout<<*it<<" "<<*itBTemp<<std::endl;
             for(deque<int>::iterator itP;itP!=powers.end();++itP){
                 products.push_back(this->_integer);
                 productEnd = multiplies_digits(this->_integer.begin(),this->_integer.end(),this->_integer.begin(),this->_integer.end(),this->_integer.begin());
