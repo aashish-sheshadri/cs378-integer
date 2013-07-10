8f8086cce097a581ce01aea9bafaec83fa3ac5dd
Aashish Sheshadri <aashish.sheshadri@gmail.com>
2013-07-10 09:04:36 -0500

    adding pow

--- Integer.h
+++ Integer.h
@@ -834,7 +834,7 @@ class Integer {
                 if(this->_sign)
                     this->_sign=!this->_sign;}
             typename C::iterator oldEnd = this->_integer.end();
-            this->_integer.resize(this->_size+rhd._size);
+            this->_integer.resize(this->_size+rhs._size);
             typename C::iterator newEnd = multiplies_digits(this->_integer.begin(),oldEnd,rhs.begin(),rhs.end(),this->_integer.begin());
             this->_size = newEnd - this->_integer.begin();
             this->_integer.resize(this->_size);
